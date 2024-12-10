a8430bc2f2ef3460f8cf37d2a67d197865cac82f
aimandanimsd <aiman.dani@msd.net.my>
2024-12-10 17:46:36 +0800

    initial commit

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <h2>Currently available ninjas</h2>
+    @if ($greeting == 'hello')
+        <p>Hi from inside the if statement</p>
+    @endif
+
+    <ul>
+        @foreach ($ninjas as $ninja)
+            <li>
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
